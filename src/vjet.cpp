41f19295e23f8334a17085c797dc5fd5401403d0
enissf-creator <enissf45@gmail.com>
2026-01-02 15:59:56 +0100

    Sa vjet i ka personi

---
+++ src/vjet.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 0;
+    cout << "S vjet i ki?";
+    cin >> x;
+    cout << "Ti i paske " << x << " vjet";
+
+    return 0;
+}
