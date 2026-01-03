1167229b26105bcc7a45a0c6ab5bfb29aa040c2e
enissf-creator <enissf45@gmail.com>
2026-01-03 18:33:45 +0100

    Update Vjet.cpp

--- src/vjet.cpp
+++ src/vjet.cpp
@@ -5,8 +5,8 @@ int main() {
     int x = 0;
     cout << "S vjet i ki?";
     cin >> x;
-    cout << "Ti i ki: " << x << " vjet";
-
+    cout << "Ti i ki: " << x << " vjet"<<endl;
+    cout <<"Ti ke lindur ne "<<2025-x<<" apo "<<2026-x;
     return 0;
 }
 
